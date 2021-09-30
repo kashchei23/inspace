53c784c9d5de5da67baef5915ca7edb771cda651
Daniel Brown <kashchei23@gmail.com>
2021-09-30 00:09:47 -0700

    Add state to App for ContextAPI implementation.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,24 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import NavBar from './navigation/NavBar';
-import Form from './form/Form';
 import Delete from './Delete';
+import DataContextWrapper from './dataContextWrapper/DataContextWrapper';
+
+import '../styles/global.scss';
 import './App.scss';
 
 const App = () => {
+	const initialQueryState = { fromDate: '', toDate: '' };
+
+	const [isDateEntered, setIsDateEntered] = useState(false);
+
+	const [query, setQuery] = useState(initialQueryState);
+
 	return (
 		<div>
 			<BrowserRouter>
-				<NavBar />
+				<NavBar setQuery={setQuery} setIsDateEntered={setIsDateEntered} />
+				<Delete />
 				<Switch>
 					<Route exact path='/'>
-						<Delete />
+						<DataContextWrapper
+							query={query}
+							isDateEntered={isDateEntered}
+							setIsDateEntered={setIsDateEntered}
+						/>
 					</Route>
-					<Route path='/apod'>
-						<Form />
-						<Delete />
-					</Route>
+					<Route path='/apod'>{/* <Delete /> */}</Route>
 				</Switch>
 			</BrowserRouter>
 		</div>
