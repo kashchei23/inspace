aa03adfdebff5f6bd167bb3ddfa9641c7380d668
Daniel Brown <kashchei23@gmail.com>
2021-10-21 10:11:01 -0700

    Add cache for response to prevent multiple calls

--- src/api/useFetch.jsx
+++ src/api/useFetch.jsx
@@ -1,27 +1,47 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 
+// import { cache } from './cache.js';
+const cache = {};
+
 export const useFetch = ({ query, isDateEntered, setIsDateEntered }) => {
+	const [fetchStatus, setFetchStatus] = useState('idle');
 	const [nasaData, setNasaData] = useState(
 		JSON.parse(localStorage.getItem('storedObj'))
 	);
 	useEffect(() => {
 		const nasaUrl = `https://api.nasa.gov/planetary/apod`;
-
+		console.log(fetchStatus);
 		const fetchData = async () => {
-			const response = await axios.get(
-				`${nasaUrl}?start_date=${query.fromDate}&end_date=${query.toDate}&api_key=${process.env.REACT_APP_API_KEY}`
-			);
-			setNasaData(response.data);
+			setFetchStatus('fetching');
+			//! need to update cache with correct query date
+			if (
+				cache[query] &&
+				cache[query].fromDate === query.fromDate &&
+				cache[query].toDate === query.toDate
+			) {
+				setNasaData(cache[query].query);
+				console.log('set from cache', cache[query].query);
+			} else {
+				const response = await axios.get(
+					`${nasaUrl}?start_date=${query.fromDate}&end_date=${query.toDate}&api_key=${process.env.REACT_APP_API_KEY}`
+				);
+				cache[query] = {
+					query: response.data,
+					fromDate: query.fromDate,
+					toDate: query.toDate,
+				};
+				setNasaData(response.data);
+				localStorage.setItem('storedObj', JSON.stringify(response.data));
+				setFetchStatus('fetched');
+				console.log('set from api', response.data);
+			}
 		};
 
-		// Fetches data on the condition that dates have been entered by the user
 		if (isDateEntered) {
 			fetchData();
-			console.log('fetched');
-			localStorage.setItem('storedObj', JSON.stringify(nasaData));
 			setIsDateEntered(false);
 		}
-	}, [query, isDateEntered, setIsDateEntered, nasaData]);
+	}, [query, isDateEntered, setIsDateEntered, nasaData, fetchStatus]);
 	return nasaData;
 };
--- src/api/useFetchCurrent.jsx
+++ src/api/useFetchCurrent.jsx
@@ -1,15 +1,21 @@
 import { useEffect } from 'react';
 import axios from 'axios';
 
+const cache = {};
 export const useFetchCurrent = (query, setCurrentPicture) => {
 	useEffect(() => {
 		const nasaUrl = `https://api.nasa.gov/planetary/apod`;
 
 		const fetchCurrentData = async () => {
-			const response = await axios.get(
-				`${nasaUrl}?start_date=${query.fromDate}&end_date=${query.toDate}&api_key=${process.env.REACT_APP_API_KEY}`
-			);
-			setCurrentPicture(response.data);
+			if (cache[query] && cache[query][0].date === query.fromDate) {
+				setCurrentPicture(cache[query]);
+			} else {
+				const response = await axios.get(
+					`${nasaUrl}?start_date=${query.fromDate}&end_date=${query.toDate}&api_key=${process.env.REACT_APP_API_KEY}`
+				);
+				cache[query] = response.data;
+				setCurrentPicture(response.data);
+			}
 		};
 		fetchCurrentData();
 	}, []);
