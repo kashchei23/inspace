fb9bb020bbf1db6b181e98aa108181ad1f2b3575
Daniel Brown <kashchei23@gmail.com>
2021-10-25 20:39:55 -0700

    Add error handling to api call

--- src/api/useFetch.jsx
+++ src/api/useFetch.jsx
@@ -5,18 +5,37 @@ const cache = {};
 
 export const useFetch = ({ query, isDateEntered, setIsDateEntered }) => {
 	const [isLoading, setIsLoading] = useState(false);
+	const [error, setError] = useState(null);
 	const [nasaData, setNasaData] = useState(
 		JSON.parse(localStorage.getItem('storedObj'))
 	);
 
 	useEffect(() => {
 		const nasaUrl = `https://api.nasa.gov/planetary/apod`;
+		const storeResponse = (res) => {
+			cache[query] = {
+				query: res.data,
+				fromDate: query.fromDate,
+				toDate: query.toDate,
+			};
+			setNasaData(res.data);
+			setIsLoading(false);
+			localStorage.setItem('storedObj', JSON.stringify(res.data));
+			console.log('set from api useFetch', res.data);
+		};
+		const handleErrors = (err) => {
+			if (err.response) {
+				setError('Sorry, no data at this URL');
+			} else if (err.request) {
+				setError('Could not fetch, please check connection');
+			} else {
+				setError('Error, please try another search');
+			}
+		};
 		const fetchData = async () => {
 			setIsLoading(true);
 			console.log('fetching');
-			console.log(cache[query]);
-			//TODO Check media type of response data.  if video, show error msg on thumnail
-			//! need to update cache with correct query date
+			//TODO Check media type of response data.  if video, show error msg on thumbnail
 			if (
 				cache[query] &&
 				cache[query].fromDate === query.fromDate &&
@@ -26,36 +45,19 @@ export const useFetch = ({ query, isDateEntered, setIsDateEntered }) => {
 				setIsLoading(false);
 				console.log('set from cache', cache[query].query);
 			} else {
-				try {
-					const response = await axios
-						.get(
-							`${nasaUrl}?start_date=${query.fromDate}&end_date=${query.toDate}&api_key=${process.env.REACT_APP_API_KEY}`
-						)
-						.then((res) => {
-							cache[query] = {
-								query: res.data,
-								fromDate: query.fromDate,
-								toDate: query.toDate,
-							};
-							setNasaData(res.data);
-							setIsLoading(false);
-							localStorage.setItem('storedObj', JSON.stringify(res.data));
-							console.log('set from api useFetch', res.data);
-						});
-				} catch (e) {
-					console.log(e);
-				}
+				const response = await axios
+					.get(
+						`${nasaUrl}?start_date=${query.fromDate}&end_date=${query.toDate}&api_key=${process.env.REACT_APP_API_KEY}`
+					)
+					.then(storeResponse)
+					.catch(handleErrors);
 			}
 		};
 
-		// fetchData();
-
 		if (isDateEntered) {
-			console.log('is date entered', isDateEntered);
 			fetchData();
 			setIsDateEntered(false);
 		}
-	}, [query, isDateEntered, nasaData]);
-	// return nasaData;
-	return { nasaData, isLoading };
+	}, [query, isDateEntered, setIsDateEntered, nasaData, error]);
+	return { nasaData, isLoading, error };
 };
