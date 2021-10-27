b10e10f27d816a1c9badccff18cb965b29ccd1af
Daniel Brown <kashchei23@gmail.com>
2021-10-27 11:29:53 -0700

    Build Layout and BottomNav components

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,17 +1,35 @@
-import React from 'react';
+import React, { useContext } from 'react';
 
 import NavBar from '../navigation/NavBar';
-import DeleteThisComponent from '../../components/DeleteThisComponent';
+import BottomNav from '../navigation/BottomNav';
+import './Layout.scss';
 import Footer from '../footer/Footer';
+import { AppContext } from '../../context/AppContext';
 
 const Layout = ({ children }) => {
+	const { navState } = useContext(AppContext);
+
+	const closeSearchAndMenu = () => {
+		navState.setSearchIsActive(false);
+		navState.setIsMenuOpen(false);
+		window.scrollTo({ top: 0, behavior: 'smooth' });
+	};
+
 	return (
-		<>
+		<div>
 			<NavBar />
-			<DeleteThisComponent />
-			{children}
+			<div className='main-container'>
+				<div
+					className={`menu-search-shadow ${
+						(navState.isMenuOpen || navState.searchIsActive) && 'fade-in'
+					}`}
+					onClick={closeSearchAndMenu}
+				/>
+				{children}
+			</div>
 			<Footer />
-		</>
+			<BottomNav />
+		</div>
 	);
 };
 export default Layout;
