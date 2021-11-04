b7d02ae2d9b9dfc29fb4700fb5b7d72680b4b647
Daniel Brown <kashchei23@gmail.com>
2021-11-03 17:46:50 -0700

    Remove deleted files

--- src/app/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render } from '@testing-library/react';
-import App from './App';
-
-test('renders App without crashes', () => {
-	render(<App />);
-	const app = <App />;
-	expect(app).toBeInTheDocument();
-});
